b402b7ce60db43090d0e4745f110facb240b4599
trooulala <trooulala@gmail.com>
2026-02-22 10:40:51 +0700

    refactor: convert mapper classes to interfaces and add jakarta.validation dependency

--- src/main/java/com/psm/book/management/mapper/BookMapper.java
+++ src/main/java/com/psm/book/management/mapper/BookMapper.java
@@ -1,7 +1,13 @@
 package com.psm.book.management.mapper;
 
+import com.psm.book.management.dto.BookInsertRequest;
+import com.psm.book.management.dto.BookResponse;
+import com.psm.book.management.entity.Book;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public class BookMapper {
+public interface BookMapper {
+    Book toEntity (BookInsertRequest dto);
+
+    BookResponse toDto (Book book);
 }
